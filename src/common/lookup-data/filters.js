ded7b4f56d8944c80de9dea5db8c10cb2d361770
athanadrian <athanadrian@gmail.com>
2022-11-13 07:53:35 +0200

    add filter to home page

---
+++ src/common/lookup-data/filters.js
@@ -0,0 +1,44 @@
+const sizes = [
+  {
+    id: '0',
+    title: '30 - 50 m2',
+    value: 0,
+  },
+  {
+    id: '1',
+    title: '50 - 70 m2',
+    value: 1,
+  },
+  {
+    id: '2',
+    title: '70 - 90 m2',
+    value: 2,
+  },
+  {
+    id: '3',
+    title: '90 - 110 m2',
+    value: 3,
+  },
+  {
+    id: '4',
+    title: '110 - 130 m2',
+    value: 4,
+  },
+  {
+    id: '5',
+    title: '130 - 150 m2',
+    value: 5,
+  },
+  {
+    id: '6',
+    title: '150 - 170 m2',
+    value: 6,
+  },
+  {
+    id: '7',
+    title: '> 170 m2',
+    value: 7,
+  },
+];
+
+export { sizes };
--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,43 +1,111 @@
-import React from 'react';
+import {
+  categories,
+  //sizes
+} from 'common/lookup-data';
+import React, { useState } from 'react';
 import AppButton from './elements/AppButton';
-
+import FormSelect from './elements/FormSelect';
+const initialFilters = {
+  type: 'rent',
+  category: '',
+  size: '',
+};
 const Filter = () => {
-  return (
-    <div className='relative flex flex-col mx-20 bottom-20 opacity-80 z-20'>
-      <div className='flex flex-row bg-light box-content w-fit rounded-t-xl cursor-pointer'>
-        <div className='px-16 py-4 font-semibold text-base text-gray-500 rounded-t-xl bg-white'>
-          Rent
-        </div>
-        <div className='px-16 py-4 font-semibold text-base text-gray-500'>
-          Buy
-        </div>
-      </div>
-      <div className='sticky flex flex-col space-y-3 justify-between bg-white p-10 rounded-r-xl rounded-bl-xl  drop-shadow-xl w-full laptop:space-y-0 laptop:flex-row'>
-        <div className='flex flex-col'>
-          <div className='text-gray-400 mb-2'>Location</div>
-          <div className='text-night font-bold text-xl'>Athens</div>
-        </div>
-        <div className='flex flex-col'>
-          <div className='text-gray-400 mb-2'>Category</div>
-          <div className='text-night font-bold text-xl'>Apartment</div>
-        </div>
-        <div className='flex flex-col'>
-          <div className='text-gray-400 mb-2'>Size</div>
-          <div className='text-night font-bold text-xl'>
-            120 m<sup>2</sup>- 150 m<sup>2</sup>
-          </div>
-        </div>
-        <div className='flex flex-col'>
-          <div className='text-gray-400 mb-2'>Price Range</div>
-          <div className='text-night font-bold text-xl'>€85,000 - €98,000</div>
-        </div>
+  const [filters, setFilters] = useState(initialFilters);
+  const {
+    type,
+    category,
+    // size
+    // squareFeet,
+    // floor,
+    // rooms,
+    // beds,
+    // bathrooms,
+    // furnished,
+    // parking,
+    // address,
+    // latitude,
+    // longitude,
+    // offer,
+    // description,
+    // regularPrice,
+    // offerPrice,
+    // images,
+  } = filters;
 
-        {/* <button className='bg-primary text-white px-7 py-3 rounded-md'>
-            Search
-          </button> */}
-        <AppButton label='Search' className='laptop:w-max w-full' />
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setFilters((preFilters) => ({ ...preFilters, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className='relative flex flex-col mx-20 bottom-20 opacity-80 z-20'>
+        <div className='flex flex-row bg-light box-content w-fit rounded-t-xl cursor-pointer'>
+          <button
+            name='type'
+            type='button'
+            value={'sale'}
+            onClick={handleChange}
+            className={`px-16 py-4 font-semibold text-base text-gray-500 rounded-t-xl bg-white
+            ${type === 'rent' ? 'bg-light text-dark' : 'bg-white text-white'}`}
+          >
+            Sale
+          </button>
+          <button
+            name='type'
+            type='button'
+            value='rent'
+            onClick={handleChange}
+            className={`px-16 py-4 font-semibold text-base text-dark rounded-t-xl
+            ${type === 'sale' ? 'bg-light' : 'bg-white'}`}
+          >
+            Rent
+          </button>
+        </div>
+        <div className='sticky flex flex-col space-y-3 justify-between bg-white p-10 rounded-r-xl rounded-bl-xl  drop-shadow-xl w-full laptop:space-y-0 laptop:flex-row'>
+          {/* <div className='flex flex-col'>
+            <div className='text-gray-400 mb-2'>Location</div>
+            <div className='text-night font-bold text-xl'>Athens</div>
+          </div> */}
+          <div className='flex flex-col'>
+            <div className='text-gray-400 mb-2'>Category</div>
+            <FormSelect
+              value={category}
+              name={'category'}
+              className='py-0 px-0 w-full shadow-none rounded bg-transparent hover:bg-white  active:bg-white focus:ring-0 focus:outline-none text-night font-bold text-xl focus:bg-white'
+              onChange={handleChange}
+              required
+              listData={categories}
+            />
+          </div>
+          {/* <div className='flex flex-col'>
+            <div className='text-gray-400 mb-2'>Size</div>
+            <FormSelect
+              value={size}
+              name={'size'}
+              className='py-0 px-0 w-full shadow-none rounded bg-transparent hover:bg-white  active:bg-white focus:ring-0 focus:outline-none text-night font-bold text-xl focus:bg-white'
+              onChange={handleChange}
+              required
+              listData={sizes}
+            /> 
+          </div>*/}
+          {/* <div className='flex flex-col'>
+            <div className='text-gray-400 mb-2'>Price Range</div>
+            <div className='text-night font-bold text-xl'>
+              €85,000 - €98,000
+            </div>
+          </div> */}
+          <AppButton label='Search' className='laptop:w-max w-full' />
+        </div>
       </div>
-    </div>
+    </form>
   );
 };
 
